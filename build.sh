6ac2193ede5ab6cc59eb1daa9bf63591ebc96550
Aditya Kshirsagar <AdityaKshirsagar631@gmail.com>
2025-07-24 10:48:40 +0530

    chore: remove build.sh, use proper playwright install

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo ">> Installing Python packages"
-pip install -r requirements.txt
-
-echo ">> Installing Chromium for Playwright"
-playwright install chromium
-
-echo ">> Build complete"
